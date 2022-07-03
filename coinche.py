354f779bc33fd180d424c1bf9a53e8af7f880643
Benoit Lebeaupin <benoit.lebeaupin@gmail.com>
2022-07-03 21:28:39 +0200

    Ajout annonces

--- coinche.py
+++ coinche.py
@@ -1,5 +1,31 @@
 import random
 
+
+class Partie:
+    # TODO: WIP
+    def __init__(self, joueurs: list[str]):
+        self.joueurs = joueurs
+        self.manche = []
+        self.score = [0,0]
+
+    def lancer_partie(self):
+        pass
+
+
+class Jeu:
+    # TODO: WIP
+    def __init(self, couleurs: list[str], valeurs: list[str]):
+        self.cartes = {couleur: {valeur: Carte(valeur,couleur) for valeur in valeurs}
+                       for couleur in couleurs}
+        self.jeu = []
+        for couleur in couleurs:
+            for valeur in valeurs:
+                self.jeu += [self.cartes[couleur][valeur]]
+
+    def melange(self):
+        return random.sample(self.jeu, 32)
+
+
 class Carte:
     def __init__(self, valeur: str, couleur: str):
         self.valeur = valeur
@@ -13,6 +39,7 @@ class Annonce:
 
 
 couleurs = ['Pique', 'Coeur', 'Carreau', 'Trefle']
+annonces_possibles = ['Pique', 'Coeur', 'Carreau', 'Trefle', 'Sans atout', 'Tout atout']
 valeurs = ['7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As']
 cartes = {couleur: {valeur: Carte(valeur,couleur) for valeur in valeurs}
           for couleur in couleurs}
@@ -164,9 +191,13 @@ def contrat_fait(annonce: Annonce, plis: list[Carte], dix_de_der: bool,
 
 
 def joue_manche(mains: list[list[Carte]], atout: str, donneur: int,
-                equipe_partie: int) -> tuple[list[Carte], bool, int]:
+                histo_annonces: list[tuple[int,Annonce]],
+                coinches: tuple[int, int, int])\
+        -> tuple[list[Carte], bool, int,
+                 list[list[tuple[int,list[Carte]]],list[Carte]]]:
+    equipe_partie = histo_annonces[-1][0] % 2
     lanceur = (donneur+1) % 4
-    historique = []
+    historique = [[],[]]
     plis_equipe_partie = []
     dix_de_der = False
     belotes = 0
@@ -175,24 +206,77 @@ def joue_manche(mains: list[list[Carte]], atout: str, donneur: int,
         for i in range(4):
             joueur = (lanceur+i) % 4
             preneur = joueur % 2 == equipe_partie
-            carte_jouee, belote = choix_carte(pli,atout,mains[joueur],
-                                              historique, preneur)
+            carte_jouee, belote = choix_carte(pli, atout, mains[joueur],
+                                              historique, preneur,
+                                              histo_annonces, coinches)
             mains[joueur].pop(mains[joueur].index(carte_jouee))
             if belote == 'belote':
-                historique += [carte_jouee]
+                historique[1] += [carte_jouee]
             elif belote == 'rebelote':
                 belotes += 1
             pli += [carte_jouee]
+        historique[0] += [(lanceur, pli)]
         lanceur = qui_prend_pli(pli, atout, lanceur)
         if lanceur % 2 == equipe_partie:
             plis_equipe_partie += pli
     if lanceur % 2 == equipe_partie:
         dix_de_der = True
-    return plis_equipe_partie, dix_de_der, belotes
+    return plis_equipe_partie, dix_de_der, belotes, historique
 
 
-def choix_carte(pli: list[Carte], atout: str, main: list[Carte],
-                historique, preneur: bool) -> tuple[Carte, str]:
+def tour_annonces(mains: list[list[Carte]], donneur: int,
+                  score: list[int,int])\
+        -> tuple[list[tuple[int,Annonce]], tuple[int, int, int]]:
+    qui_parle = (donneur + 1) % 4
+    historique_annonces = []
+    annonce, passe = choix_annonce(donneur, mains[qui_parle], score,
+                                   historique_annonces, qui_parle)
+    coinche = False
+    qui_coinche = -1
+    if not passe:
+        historique_annonces += [(qui_parle, annonce)]
+        for i in [1,3]:
+            qui_coinche = (qui_parle + 1) % 4
+            coinche = choix_coinche(donneur, mains[qui_coinche], score,
+                                    historique_annonces, qui_coinche)
+            if coinche:
+                break
+    n_passe = 0
+    while n_passe < 3 and not coinche:
+        qui_parle = (qui_parle + 1) % 4
+        annonce, passe = choix_annonce(donneur, mains[qui_parle], score,
+                                       historique_annonces, qui_parle)
+        if not passe:
+            n_passe = 0
+            historique_annonces += [(qui_parle, annonce)]
+            for i in [1, 3]:
+                qui_coinche = (qui_parle + 1) % 4
+                coinche = choix_coinche(donneur, mains[qui_coinche], score,
+                                        historique_annonces, qui_coinche)
+                if coinche:
+                    break
+        else:
+            n_passe += 1
+    qui_surcoinche = -1
+    surcoinche = False
+    if coinche:
+        for i in [0, 2]:
+            qui_surcoinche = (qui_parle + i) % 4
+            surcoinche = choix_surcoinche(donneur, mains[qui_surcoinche],
+                                          score, historique_annonces,
+                                          qui_surcoinche)
+            if surcoinche:
+                break
+    coinches = (coinche+surcoinche, qui_coinche, qui_surcoinche)
+    return historique_annonces, coinches
+
+
+def choix_carte(pli: list[Carte],
+                atout: str, main: list[Carte],
+                historique, preneur: bool,
+                histo_annonces: list[tuple[int,Annonce]],
+                coinches: tuple[int, int, int])\
+        -> tuple[Carte, str]:
     cartes_possibles = cartes_posables(pli, atout, main)
     carte_jouee = random.choice(cartes_possibles)
     belote = ''
@@ -200,8 +284,8 @@ def choix_carte(pli: list[Carte], atout: str, main: list[Carte],
         if (carte_jouee.valeur == 'Dame' and cartes[atout]['Roi'] in main or
                 carte_jouee.valeur == 'Roi' and cartes[atout]['Dame'] in main):
             belote = 'belote'
-        elif (carte_jouee.valeur == 'Dame' and cartes[atout]['Roi'] in historique or
-              carte_jouee.valeur == 'Roi' and cartes[atout]['Dame'] in historique):
+        elif (carte_jouee.valeur == 'Dame' and cartes[atout]['Roi'] in historique[1] or
+              carte_jouee.valeur == 'Roi' and cartes[atout]['Dame'] in historique[1]):
             belote = 'rebelote'
     elif preneur and atout == 'Tout atout':
         coul = carte_jouee.couleur
@@ -209,19 +293,70 @@ def choix_carte(pli: list[Carte], atout: str, main: list[Carte],
                 carte_jouee.valeur == 'Roi' and cartes[coul]['Dame'] in main):
             if not pli or pli[0].couleur == coul:
                 belote = 'belote'
-        elif (carte_jouee.valeur == 'Dame' and cartes[coul]['Roi'] in historique or
-              carte_jouee.valeur == 'Roi' and cartes[coul]['Dame'] in historique):
+        elif (carte_jouee.valeur == 'Dame' and cartes[coul]['Roi'] in historique[1] or
+              carte_jouee.valeur == 'Roi' and cartes[coul]['Dame'] in historique[1]):
             if not pli or pli[0].couleur == coul:
                 belote = 'rebelote'
     return carte_jouee, belote
 
 
-jeu_melange = random.sample(jeu, 32)
-mains = [jeu_melange[0:8],jeu_melange[8:16],
-         jeu_melange[16:24],jeu_melange[24:32]]
-atout = random.choice(couleurs + ['Tout atout', 'Sans atout'])
+def choix_annonce(donneur: int, main: list[Carte], score: list[int,int],
+                  historique_annonces: list[tuple[int,Annonce]],
+                  qui_parle: int) -> tuple[Annonce,bool]:
+    if not historique_annonces:
+        contrat_min = 80
+    else:
+        contrat_min = historique_annonces[-1][1].contrat + 10
+    if contrat_min == 260:
+        passe = True
+        annonce = Annonce('NA',-1)
+    else:
+        prob_passe = int(((contrat_min - 70)/10)*2)
+        passe = bool(random.randint(0,prob_passe))
+        if not passe:
+            coul = random.choice(annonces_possibles)
+            surenchere = random.choice([0,10,20,30])
+            contrat = contrat_min + surenchere
+            if contrat > 180:
+                contrat = 250
+            passe = False
+            annonce = Annonce(coul,contrat)
+        else:
+            passe = True
+            annonce = Annonce('NA', -1)
+    return annonce, passe
+
+
+def choix_coinche(donneur, main, score, historique_annonces, qui_coinche)\
+        -> bool:
+    return random.randint(0,30) == 30
+
+
+def choix_surcoinche(donneur, main, score, historique_annonces,
+                     qui_surcoinche) -> bool:
+    return random.randint(0,10) == 10
+
 donneur = random.randint(0,3)
-equipe_partie = random.randint(0,1)
-plis_equipe_partie, dix_de_der, belotes = joue_manche(mains, atout, donneur, equipe_partie)
-points_faits = comptage_points(plis_equipe_partie, atout, dix_de_der, belotes)
-print(points_faits)
+score = [0,0]
+manches = []
+while score[0] <= 1000 and score[1] <= 1000:
+    histo_annonces = []
+    while not histo_annonces:
+        donneur = (donneur + 1) % 4
+        jeu_melange = random.sample(jeu, 32)
+        mains = [jeu_melange[0:8], jeu_melange[8:16],
+                 jeu_melange[16:24], jeu_melange[24:32]]
+        histo_annonces, coinches =\
+            tour_annonces(mains, donneur, score)
+    atout = histo_annonces[-1][1].couleur
+    plis_equipe_partie, dix_de_der, belotes, historique =\
+        joue_manche(mains, atout, donneur, histo_annonces, coinches)
+    equipe_partie = histo_annonces[-1][0] % 2
+    points_en_jeu = histo_annonces[-1][1].contrat*(coinches[0]+1)
+    if contrat_fait(histo_annonces[-1][1], plis_equipe_partie,
+                    dix_de_der, belotes):
+        score[equipe_partie] += points_en_jeu
+    else:
+        score[(equipe_partie+1) % 2] += points_en_jeu
+    manches += [(histo_annonces, historique)]
+#   print(score)
